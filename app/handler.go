a72bcd56c9d1b55aa56448c84f8cf4bfadb830a2
Pratik Kulkarni <pnkulkarni@proton.me>
2024-09-08 19:12:33 +0530

    codecrafters submit [skip ci]

--- app/handler.go
+++ app/handler.go
@@ -59,40 +59,58 @@ func (r *RedisServer) handleGetCommand(args []RESP) []byte {
 	if len(args) != 1 {
 		return []byte("-ERR wrong number of arguments for 'get' command\r\n")
 	}
+
 	key := args[0].String
+
 	r.mu.RLock()
-	value, exists := r.store[key]
-	r.mu.RUnlock()
-	if !exists {
+	defer r.mu.RUnlock()
+
+	if expiryTime, exists := r.expiry[key]; exists && time.Now().After(expiryTime) {
+		r.mu.RUnlock()
+		r.mu.Lock()
+		delete(r.store, key)
+		delete(r.expiry, key)
+		r.mu.Unlock()
+		r.mu.RLock()
 		return []byte("$-1\r\n")
 	}
-	response := fmt.Sprintf("$%d\r\n%s\r\n", len(value), value)
 
-	return []byte(response)
+	if value, exists := r.store[key]; exists {
+		return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(value), value))
+	}
+
+	return []byte("$-1\r\n")
 }
 
 func (r *RedisServer) handleSetCommand(args []RESP) []byte {
 	if len(args) < 2 {
 		return []byte("-ERR wrong number of arguments for 'set' command\r\n")
 	}
+
 	key := args[0].String
 	value := args[1].String
 
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	r.store[key] = value
-	delete(r.expiry, key)
+	expiryDuration := time.Duration(0)
 
-	if len(args) == 4 {
+	if len(args) > 2 {
 		option := strings.ToUpper(args[2].String)
-		if option == "EX" {
-			expirySeconds, err := strconv.Atoi(args[3].String)
+		if option == "PX" && len(args) > 3 {
+			ms, err := strconv.Atoi(args[3].String)
 			if err != nil {
-				return []byte("-ERR invalid expiry time\r\n")
+				return []byte("-ERR PX value is not a valid integer\r\n")
 			}
-			r.expiry[key] = time.Now().Add(time.Duration(expirySeconds) * time.Second)
+			expiryDuration = time.Duration(ms) * time.Millisecond
 		}
 	}
+
+	r.mu.Lock()
+	r.store[key] = value
+	if expiryDuration > 0 {
+		r.expiry[key] = time.Now().Add(expiryDuration)
+	} else {
+		delete(r.expiry, key)
+	}
+	r.mu.Unlock()
 
 	return []byte("+OK\r\n")
 }
